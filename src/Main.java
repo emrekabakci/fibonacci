205f81a63ccd1632fdc187b15fc8afbec4be7161
emrekabakci <emrekabakci.2@icloud.com>
2023-12-16 11:23:25 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //New scanner created.
         Scanner inp = new Scanner(System.in);
 
+        //Variables defined.
         int a = 0;
         int b = 1;
         int c;
 
+        //Data retrieved from user.
         System.out.print("Enter number : ");
         int n = inp.nextInt();
 
+        //Fibonacci sequence is always starting with 0 and 1 that's why we printed 0 and 1 first.
         System.out.print("The fibonacci sequence is: " + a + ", " + b);
 
+        //Continuation of the fibonacci sequence printed.
         for (int i = 2; i < n; i++) {
             c = a + b;
             System.out.print(", " + c);
